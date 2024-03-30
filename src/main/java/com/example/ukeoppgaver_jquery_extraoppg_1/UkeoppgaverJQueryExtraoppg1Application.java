ad89b6c181af1e9e1e3eeb3a6ef56fd052dfec93
47462 <coskun.aydn@gmail.com>
2024-03-30 13:33:09 +0100

    new

---
+++ src/main/java/com/example/ukeoppgaver_jquery_extraoppg_1/UkeoppgaverJQueryExtraoppg1Application.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_jquery_extraoppg_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeoppgaverJQueryExtraoppg1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeoppgaverJQueryExtraoppg1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukeoppgaver_jquery_extraoppg_1/UkeoppgaverJQueryExtraoppg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_jquery_extraoppg_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkeoppgaverJQueryExtraoppg1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
